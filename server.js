20ca817e248518eead22a6b11d3def2746803c2f
RCThakur <rinkeshthakur1997@gmail.com>
2025-07-18 04:42:09 +0530

    Update folder structure

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // Connect to MongoDB
 connectDB();
